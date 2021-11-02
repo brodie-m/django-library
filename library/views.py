bfa73fb6f673334bffde51087cea767fd857f286
brodie-m <brodiedmcguire@gmail.com>
2021-11-02 15:08:25 +0000

    can return books

--- library/views.py
+++ library/views.py
@@ -12,16 +12,20 @@ def show(req):
     return render(req, 'library/all-books.html',context)
 @login_required
 def showbyid(req,id):
+    book_to_save = Book.objects.get(id=id)
     if req.method == 'POST':
         form = BorrowBookForm(req.POST)
-        book_to_save = Book.objects.get(id=id)
-        if form.is_valid():
+        
+        if form.is_valid() and not book_to_save.borrower:
             book_to_save.borrower = form.cleaned_data['borrower']
             book_to_save.save()
             return redirect('books-show')
+        else:
+            book_to_save.borrower = None
+            book_to_save.save()
     else:
         form = BorrowBookForm()
-    data = {'form': form, 'books' : Book.objects.filter(id=id)}
+    data = {'form': form, 'books' : Book.objects.filter(id=id), 'book_to_save':book_to_save}
     
     return render(req, 'library/book.html', data)
 def new_book(req):
